6f35365bd23c793e4587a8ea7ff6bae9e19be739
Victor Palade <victor@cloudflavor.io>
2026-03-13 10:04:56 +0100

    remove .opal dir
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

--- assets/.opal/scripts/install-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-cd /workspace
-
-echo "[global before]"
-apt-get update && apt-get -y upgrade
-mkdir -p vendor
-echo "dependency-output" > vendor/deps.txt
-echo "[global after]"
